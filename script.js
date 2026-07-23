24e2570b266d58a1c78219fccccb4b9468a90589
Yarkanofficial <Yarkanofficial@gmail.com>
2026-07-23 12:07:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if(target){
+            target.scrollIntoView({
+                behavior:"smooth"
+            });
+        }
+    });
+});
+
+// Hero Animation
+window.addEventListener("load",()=>{
+
+    document.querySelector(".hero").classList.add("show");
+
+});
+
+// Buy Button
+const buyBtn=document.querySelector(".btn");
+
+if(buyBtn){
+
+buyBtn.addEventListener("click",(e)=>{
+e.preventDefault();
+
+alert("YARKAN (YKN) Launching Soon 🚀");
+
+});
+
+}
