faa264f3d1614656e24dd6d0aee487b9d2e4c6e7
Yarkanofficial <Yarkanofficial@gmail.com>
2026-07-23 10:45:09 +0300

    Add event listener for buy button alert

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    const buyButton = document.querySelector(".btn");
+
+    buyButton.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        alert("YARKAN (YKN) is coming soon! Stay tuned.");
+    });
+
+});
